5ce5c05c300e24109e204d1517a8fc20eb9c4595
Pranay2319 <pranbhadmvlu23@gmail.com>
2026-07-16 14:15:10 +0530

    Add Node.js practical examples for JavaScript
    Added various Node.js examples demonstrating basic JavaScript commands, functions, operators, control statements, and loops.

---
+++ PRACTICAL 1.js
@@ -0,0 +1,91 @@
+b. Create a simple Node.js file and run it using the terminal.
+Code:
+console.log("Hello, Welcome to first practical of MERN ");
+console.log("Pranay");
+
+c. Write a Node.js script demonstrating basic JavaScript commands
+(console log, variables, and arithmetic operations).
+Code:
+let a=10;
+let b=25;
+console.log("Addition:",a+b);
+console.log("Subtraction:",a-b);
+
+console.log("Multiplication:",a*b);
+console.log("Division:",a/b);
+console.log("Pranay T003")
+
+d. Create and run a simple program using variables and functions.
+Code:
+let a=10;
+let b=25;
+function add(a,b){
+return a + b;
+}
+function sub(a,b){
+return a - b;
+}
+console.log("First number:",a);
+console.log("Second number:",b);
+console.log("Addition:",add(a,b));
+console.log("Subtraction:",sub(a,b));
+console.log("Pranay T003")
+
+e. Demonstrate use of operators and control statements in JavaScript
+Code:
+let a = 20;
+let b = 10;
+// Comparison Operators
+
+console.log("\nComparison Operators");
+console.log("a > b :", a > b);
+console.log("a < b :", a < b);
+console.log("a == b :", a == b);
+console.log("a != b :", a != b);
+// if...else Statement
+console.log("\nIf-Else Statement");
+if (a > b) {
+console.log("a is greater than b");
+} else {
+console.log("b is greater than or equal to a");
+}
+// switch Statement
+console.log("\nSwitch Statement");
+let day = 2;
+switch(day) {
+case 1:
+console.log("Monday");
+break;
+case 2:
+console.log("Tuesday");
+break;
+case 3:
+console.log("Wednesday");
+break;
+case 4:
+console.log("Thursday");
+break;
+case 5:
+console.log("Friday");
+break;
+case 6:
+console.log("Saturday");
+break;
+case 7:
+console.log("Sunday");
+break;
+
+default:
+console.log("Invalid Day");
+}
+console.log("Pranay T003");
+
+f. Write programs using loops and template literals.
+Code:
+let number = 5;
+console.log(`Multiplication Table of ${number}`);
+for (let i = 1; i <= 10; i++) {
+console.log(`${number} x ${i} = ${number * i}`);
+}
+console.log("Pranay T003")
+
